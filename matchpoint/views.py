653eab62e41566a03d6b08f7ca4e78bf36164c31
James Socol <me@jamessocol.com>
2013-04-10 16:55:53 -0400

    Nearly full API.

--- matchpoint/views.py
+++ matchpoint/views.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
 import json
+from datetime import datetime
+from functools import wraps
 
 from flask import jsonify, request, make_response
 
@@ -11,19 +13,59 @@ from matchpoint.models import Namespace
 JSON = 'application/json'
 
 
+# TODO: Move.
+def jsonify(f):
+    @wraps(f)
+    def _wrapped(*a, **kw):
+        ret = f(*a, **kw)
+        if isinstance(ret, tuple):
+            if len(ret) == 2:
+                data, status = ret
+                headers = {}
+            elif len(ret) == 3:
+                data, status, headers = ret
+            else:
+                data = ret
+                status = 200
+                headers = {}
+        else:
+            data = ret
+            status = 200
+            headers = {}
+        headers['Content-Type'] = JSON
+        return json.dumps(data), status, headers
+    return _wrapped
+
+
 @app.route('/api/v1/<name>', methods=['GET', 'HEAD'])
+@jsonify
 def get_namespace(name):
     ns = Namespace.query().filter(Namespace.name == name).first()
 
     if ns is None:
-        return '', 404, {'Content-Type': JSON}
+        return None, 404
+
+    total_interests = len(ns.interests)
+    since = request.if_modified_since
+    if since is None:
+        since = datetime.min
 
     fmt = lambda s: '/'.join((ns.name, s))
-    interests = dict(((fmt(i.name), i.current.to_dict()) for i in ns.interests))
+    interests = dict(((fmt(i.name), i.current.to_dict()) for i
+                      in ns.interests if i.modified >= since))
+
+    matched_interests = len(interests)
+
+    if matched_interests == 0:
+        return {}, 304, {}
+
+    if matched_interests < total_interests:
+        status = 206
+    else:
+        status = 200
 
     headers = {
-        'Content-Type': JSON,
         'Last-Modified': ns.modified.isoformat() + 'Z',
     }
 
-    return json.dumps(interests), 200, headers
+    return interests, status, headers
